b54b1c66a41225a0ceb4faf7862591e5e6e47660
Nbanksolo <Nbanksolotech@gmail.com>
2022-05-16 11:53:14 +0800

    apps/_root/next.config.mjs

---
+++ apps/_root/next.config.mjs
@@ -0,0 +1,75 @@
+import transpileModules from 'next-transpile-modules'
+
+const withTranspileModules = transpileModules(['@sushiswap/ui', '@sushiswap/chain'])
+
+const { BLOG_URL, ANALYTICS_URL, DOCS_URL, FURO_URL, LEGACY_URL } = process.env
+
+export default withTranspileModules({
+  reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord{/}?',
+        permanent: true,
+        destination: 'https://discord.gg/SDPH8SNVZW',
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/analytics',
+        destination: `${ANALYTICS_URL}/analytics`,
+      },
+      {
+        source: '/analytics/:path*',
+        destination: `${ANALYTICS_URL}/analytics/:path*`,
+      },
+      {
+        source: '/blog',
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${BLOG_URL}/blog/:path*`,
+      },
+      {
+        source: '/docs',
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: '/docs/:path*',
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+      {
+        source: '/furo',
+        destination: `${FURO_URL}/furo`,
+      },
+      {
+        source: '/furo/:path*',
+        destination: `${FURO_URL}/furo/:path*`,
+      },
+      // {
+      //   source: '/store',
+      //   destination: `${STORE_URL}/store`,
+      // },
+      // {
+      //   source: '/store/:path*',
+      //   destination: `${STORE_URL}/store/:path*`,
+      // },
+      {
+        source: '/legacy',
+        destination: `${LEGACY_URL}/legacy`,
+      },
+      {
+        source: '/legacy/:path*',
+        destination: `${LEGACY_URL}/legacy/:path*`,
+      },
+    ]
+  },
+})
